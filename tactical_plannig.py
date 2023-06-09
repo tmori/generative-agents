8ab5a84e2bee3a6241a46f901f126b7fdb16c6b3
Takashi Mori <kanetugu2015@gmail.com>
2023-06-09 15:31:01 +0900

    bug fixed

--- tactical_plannig.py
+++ tactical_plannig.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from plan import Plan
-from db_manager import get_qa
 from prompt_template import PromptTemplate
+from db_manager import get_qa
 
 class TacticalPlanning:
     def __init__(self, plan: Plan, db_dir: str, summary_template_path: str):
@@ -16,15 +16,17 @@ class TacticalPlanning:
         if (len(prioritized_plan) == 0):
             return None
         
-        row = prioritized_plan.head(1)
-        plan_id = row["PlanID"][0]
+        #print(prioritized_plan)
+        row = prioritized_plan.head(1).iloc[0]
+        #print(row)
+        plan_id = row["PlanID"]
         self.plan.update_status_doing(plan_id)
 
-        document_id = row["DocumentID"][0]
-        purpose = row["InvestigationPurpose"][0]
-        perspectives = row["InvestigationPerspectives"][0]
+        document_id = row["DocumentID"]
+        purpose = row["InvestigationPurpose"]
+        perspectives = row["InvestigationPerspectives"]
 
-        return (row["PlanID"][0], document_id, self._generate_document_question(prompt_templates, document_id, purpose, perspectives))
+        return (plan_id, document_id, self._generate_document_question(prompt_templates, document_id, purpose, perspectives))
 
     def _prioritize_plan(self):
         plan_data = self.plan.get_data()
@@ -38,12 +40,9 @@ class TacticalPlanning:
             query = prompt_query_template.get_prompt(document_id=document_id, purpose=purpose, perspectives=perspectives)
             return query
         else:
-            qa = get_qa(self.db_dir, document_id)
             prompt_query_template = PromptTemplate(prompt_template_path)
             query = prompt_query_template.get_prompt(document_id=document_id, purpose=purpose, perspectives=perspectives)
-            print({"question": query})
-            reply = qa({"question": query})
-            return reply["answer"]
+            return query
 
 if __name__ == "__main__":
     from query import Query
@@ -52,21 +51,24 @@ if __name__ == "__main__":
     plan.load_from_json("./test/plan.json")
     db_dir = "../tutorial_langchain/dbs"
     tactical_planning = TacticalPlanning(plan, db_dir, "./prompt_templates/ptemplate_subq_summary.txt")
-
-    ret = tactical_planning.generate_question("./prompt_templates/ptemplate_subq_detail.txt")
-
-    plan_id = ret[0]
-    doc_id = ret[1]
-    question = ret[2]
-    qa = get_qa(db_dir, doc_id)
     memory_stream = MemoryStream()
-    prompt_template_path = "./prompt_templates/ptemplate_query.txt"
-    query = Query(doc_id, question, memory_stream, qa)
-    memory_id = query.run(prompt_template_path, question)
-    print("REPLY: " + memory_stream.get_reply())
-    print("POINT: " + str(memory_stream.get_point()))
-    memory_stream.save_to_json("test/memory.json")
 
-    plan.update_status_done(plan_id, memory_id)
-    plan.save_to_json("./test/updated_plan.json")
+    while True:
+        ret = tactical_planning.generate_question("./prompt_templates/ptemplate_subq_detail.txt")
+        if ret == None:
+            print("END")
+            break
+        plan_id = ret[0]
+        doc_id = ret[1]
+        question = ret[2]
+        qa = get_qa(db_dir, doc_id)
+        prompt_template_path = "./prompt_templates/ptemplate_query.txt"
+        query = Query(doc_id, question, memory_stream, qa)
+        memory_id = query.run(prompt_template_path, question)
+        print("REPLY: " + memory_stream.get_reply())
+        print("POINT: " + str(memory_stream.get_point()))
+        memory_stream.save_to_json("test/memory.json")
+
+        plan.update_status_done(plan_id, memory_id)
+        plan.save_to_json("./test/updated_plan.json")
 
