dc8b37cdf140d12c68400408a075b061def99a24
Takashi Mori <kanetugu2015@gmail.com>
2023-06-12 15:52:37 +0900

    add strategy history

--- planner.py
+++ planner.py
@@ -6,13 +6,20 @@ import json
 from prompt_template import PromptTemplate
 from question import get_response
 from plan import Plan
+import os
 
 class Planner:
-    def __init__(self, main_question, mission_path, strategy_path, query_plan_path):
+    def __init__(self, main_question, mission_path, strategy_path, query_plan_path, strategy_history_path):
         self.main_question = main_question
         self.mission_path = mission_path
         self.strategy_path = strategy_path
         self.query_plan_path = query_plan_path
+        self.strategy_history_path = strategy_history_path
+        if os.path.exists(strategy_history_path):
+            with open(strategy_history_path, 'r') as file:
+                self.strategy_history_json = json.load(file)
+        else:
+            self.strategy_history_json = {}
         self.plan = Plan()
 
     def generate_query(self, document_list, history):
@@ -23,13 +30,17 @@ class Planner:
         self.strategy = pstrategy.get_prompt()
 
         pquery_plan = PromptTemplate(self.query_plan_path)
+        past_strategies = []
+        if "Strategies" in self.strategy_history_json:
+            past_strategies = self.strategy_history_json["Strategies"]
+
         self.query_plan = pquery_plan.get_prompt(
             MainQuestion = self.main_question,
             Mission = self.mission,
             Strategy = self.strategy,
             DocumentList = document_list,
             History = history,
-            PastStrategies = []
+            PastStrategies = past_strategies
         )
         print(self.query_plan)
 
@@ -38,12 +49,20 @@ class Planner:
         print(self.reply_raw)
         self.reply_json = json.loads(self.reply_raw)
         self.plan.set_strategy(self.reply_json["DetailedStrategy"])
+        if "Strategies" not in self.strategy_history_json:
+            self.strategy_history_json["Strategies"] = []
+        self.strategy_history_json["Strategies"].append(self.reply_json["DetailedStrategy"])
+
         for entry in self.reply_json["Plan"]:
             self.plan.add_data(entry["DocumentID"], entry["Purpose"], entry["Perspectives"])
 
     def save_to_json(self, file_path):
         with open(file_path, 'w') as file:
             json.dump(self.reply_json, file, indent=4, ensure_ascii=False)
+
+    def save_strategy_history(self):
+        with open(self.strategy_history_path, 'w') as file:
+            json.dump(self.strategy_history_json, file, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
     import sys
@@ -60,10 +79,12 @@ if __name__ == "__main__":
         main_question = main_question,
         mission_path= "./prompt_templates/ptemplate_mission.txt",
         strategy_path= "./prompt_templates/ptemplate_strategy.txt",
-        query_plan_path= "./prompt_templates/ptemplate_query_plan.txt"
+        query_plan_path= "./prompt_templates/ptemplate_query_plan.txt",
+        strategy_history_path="./test/strategy_history.json"
     )
     for doc_list in batched_list:
         planner.generate_query(doc_list, "")
         planner.create_plan()
         planner.save_to_json("reply.json")
     planner.plan.save_to_json("test/plan.json")
+    planner.save_strategy_history()
