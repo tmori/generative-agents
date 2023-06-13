6e3080091513cbe3c7f0959a44f2b04d2d325404
Takashi Mori <kanetugu2015@gmail.com>
2023-06-13 15:00:30 +0900

    spec changed

--- do_query.bash
+++ do_query.bash
@@ -2,7 +2,7 @@
 
 if [ $# -ne 1 ]
 then
-    echo "Usage: $0 <query.txt>"
+    echo "Usage: $0 <query_dir>"
     exit 1
 fi
 
@@ -18,8 +18,22 @@ then
 else
     mkdir test/result
 fi
-query="`cat $1`"
+query_dir=$1
+query="`cat ${query_dir}/query.txt`"
+
+rm -rf test/result/*
+rm -rf test/*.json
+
+TRY_NO=1
 python3 planner.py "$query" ../documents/document.list 
 python3 tactical_plannig.py
 python3 evaluator.py "$query" ./test/result/updated_plan.json ./test/result/memory.json
 
+dir_name=q_${TRY_NO}
+if [ -d ${query_dir}/${dir_name} ]
+then
+    :
+else
+    mkdir ${query_dir}/${dir_name}
+fi
+cp -rp test/* ${query_dir}/${dir_name}/
