542e0a58f5022c2be6cc6cbd8629394fb7657a47
Takashi Mori <kanetugu2015@gmail.com>
2023-06-21 11:39:45 +0900

    refactored

--- db_manager.py
+++ db_manager.py
@@ -19,14 +19,7 @@ def get_similarity_search_with_average_score(db_dir: str, doc_id: str, terms: st
     average_score = scores_series.mean()
     return average_score
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("USAGE: " + sys.argv[0] + " <db_dir> <doclist> <terms>")
-        sys.exit(1)
-    
-    db_dir=sys.argv[1]
-    doclist=sys.argv[2]
-    terms=sys.argv[3]
+def get_similarity_search_results(doclist: str, db_dir: str, terms: str, top_k: int):
     with open(doclist, "r") as file:
         lines = file.readlines()
         total_list = [line.strip() for line in lines]
@@ -34,10 +27,24 @@ if __name__ == "__main__":
     scores = []
     entries = []
     for entry in total_list:
-        score = get_similarity_search_with_average_score(db_dir, entry, terms, 10)
+        score = get_similarity_search_with_average_score(db_dir, entry, terms, top_k)
         scores.append(score)  
         entries.append(entry)
     
-    df = pd.DataFrame({'entry': entries, 'score': scores})
-    top_5 = df.sort_values(by='score', ascending=True).head(5)
-    print(top_5)
+    df = pd.DataFrame({'title': entries, 'score': scores})
+    top = df.sort_values(by='score', ascending=True).head(top_k)['title'].tolist()
+    return top
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("USAGE: " + sys.argv[0] + " <db_dir> <doclist> <terms> <num>")
+        sys.exit(1)
+    
+    db_dir=sys.argv[1]
+    doclist=sys.argv[2]
+    terms=sys.argv[3]
+    num=int(sys.argv[4])
+
+    docs = get_similarity_search_results(doclist, db_dir, terms, num)
+    print(docs)
---
+++ tools/get_terms.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <reflection path>"
+	exit 1
+fi
+
+reflection_path=${1}
+
+grep Term ${reflection_path}  | awk -F: '{print $2}' | sed 's/\"//g'| sed ':a;N;$!ba;s/\n//g'
